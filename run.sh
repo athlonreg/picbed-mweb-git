670ac81bb86a90275bd7634f2f528eaeeb7c715f
Canvas <15563836030@163.com>
2018-12-02 12:28:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # -*- coding: UTF-8 -*-
-cd /Users/canvas/Documents/Tower/picbed-mweb-git && git checkout coding
+cd /Users/canvas/Documents/Tower/picbed-mweb-git && git checkout master
 nohup python3 main_upimg.py &
